aed70ff08ed982ce3357be846e8feb848ad3ed39
natp0ng <nattapong.kongmun@gmail.com>
2020-08-31 14:20:55 +0700

    rebuilding site Mon Aug 31 14:20:55 +07 2020

--- content/posts/my-first-post.md
+++
@@ -1,7 +0,0 @@
----
-title: "My First Post"
-date: 2020-08-31T13:52:57+07:00
-draft: true
----
-
-# Hello
--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,10 @@ set -e
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+# hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
-cd public
+# cd public
 
 # Add changes to git.
 git add .
@@ -21,5 +21,7 @@ if [ -n "$*" ]; then
 fi
 git commit -m "$msg"
 
+git branch --set-upstream-to origin master
+
 # Push source and build repos.
-git push -u origin master
+git push origin master
